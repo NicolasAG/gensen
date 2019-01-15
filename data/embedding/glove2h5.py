91ce835a73e222cdc0a957409243b19bce982fbb
Nicolas Gontier <gontier.nicolas@yahoo.fr>
2019-01-15 11:15:52 -0500

    up-to-date

--- data/embedding/glove2h5.py
+++ data/embedding/glove2h5.py
@@ -2,6 +2,8 @@
 import h5py
 import numpy as np
 
+print("grabbing glove vocab & vectors...")
+
 glove_vectors = [
     line.strip().split()
     for line in open('glove.840B.300d.txt', 'r')
@@ -13,7 +15,11 @@ vectors = np.array(
 ).astype(np.float32)
 vocab = '\n'.join(vocab)
 
+print("creating h5py file...")
+
 f = h5py.File('glove.840B.300d.h5', 'w')
 f.create_dataset(data=vectors, name='embedding')
 f.create_dataset(data=vocab, name='words_flatten')
 f.close()
+
+print("done.")
