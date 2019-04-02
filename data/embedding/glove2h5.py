29618bf394610c147f79fc3a9d2a5f206252d637
Nicolas Gontier <nicolas.gontier@element.ai>
2019-04-02 20:09:17 +0000

    debug glove reader

--- data/embedding/glove2h5.py
+++ data/embedding/glove2h5.py
@@ -5,8 +5,7 @@ import numpy as np
 print("grabbing glove vocab & vectors...")
 
 glove_vectors = [
-    line.strip().split()
-    for line in open('glove.840B.300d.txt', 'r')
+    line.decode('utf-8').strip().split() for line in open('glove.840B.300d.txt', 'r')
 ]
 
 vocab = [line[0] for line in glove_vectors]
