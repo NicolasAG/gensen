92f6630c101e9577ce1796dea6ebd169e7e4561b
Nicolas Gontier <nicolas.gontier@element.ai>
2019-04-02 18:49:34 +0000

    report download status in download files

--- data/embedding/glove2h5.sh
+++ data/embedding/glove2h5.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+echo "downloading glove zip file ..."
 # curl -LO 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
 wget -q http://nlp.stanford.edu/data/glove.840B.300d.zip
 unzip glove.840B.300d.zip
 rm glove.840B.300d.zip
+echo "done."
 python glove2h5.py
--- data/models/download_models.sh
+++ data/models/download_models.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+echo "downloading gensen model nli_large_bothskip_2layer ..."
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer_vocab.pkl
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer.model
+echo "downloading gensen model nli_large_bothskip_parse ..."
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse_vocab.pkl
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse.model
+echo "downloading gensen model nli_large_bothskip ..."
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_vocab.pkl
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip.model
+echo "downloading gensen model nli_large ..."
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_vocab.pkl
 wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large.model
+echo "done."
