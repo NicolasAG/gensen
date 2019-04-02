2cfc50b3e70a9499eb862e8a4b3fffa432586c83
Nicolas Gontier <nicolas.gontier@element.ai>
2019-04-02 18:43:52 +0000

    reduce output logs from download files

--- data/embedding/glove2h5.sh
+++ data/embedding/glove2h5.sh
@@ -1,4 +1,6 @@
-curl -LO 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
+#!/usr/bin/env bash
+# curl -LO 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
+wget -q http://nlp.stanford.edu/data/glove.840B.300d.zip
 unzip glove.840B.300d.zip
 rm glove.840B.300d.zip
 python glove2h5.py
--- data/models/download_models.sh
+++ data/models/download_models.sh
@@ -1,8 +1,9 @@
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer_vocab.pkl
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer.model
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse_vocab.pkl
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse.model
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_vocab.pkl
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip.model
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large_vocab.pkl
-wget https://genseniclr2018.blob.core.windows.net/models/nli_large.model
+#!/usr/bin/env bash
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer_vocab.pkl
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer.model
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse_vocab.pkl
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse.model
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_vocab.pkl
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip.model
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_vocab.pkl
+wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large.model
