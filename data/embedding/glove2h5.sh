2da9fcf627144f9b4449c5a942fa6b0431bed303
NicolasAG <gontier.nicolas@yahoo.fr>
2019-05-23 14:48:46 -0400

    make setup script smarter

--- data/embedding/glove2h5.sh
+++ data/embedding/glove2h5.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-echo "downloading glove zip file ..."
-# curl -LO 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
-wget -q http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip glove.840B.300d.zip
-rm glove.840B.300d.zip
-echo "done."
-python glove2h5.py
+
+if [[ ! -f "./glove.840B.300d.txt" ]]
+then
+    echo "downloading glove zip file ..."
+    curl -LO 'http://nlp.stanford.edu/data/glove.840B.300d.zip'
+    wget -q http://nlp.stanford.edu/data/glove.840B.300d.zip
+    unzip glove.840B.300d.zip
+    rm glove.840B.300d.zip
+    echo "done."
+fi
+
+if [[ ! -f "./glove.840B.300d.h5" ]]
+then
+    python glove2h5.py
+fi
--- data/models/download_models.sh
+++ data/models/download_models.sh
@@ -1,14 +1,49 @@
 #!/usr/bin/env bash
-echo "downloading gensen model nli_large_bothskip_2layer ..."
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer_vocab.pkl
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer.model
-echo "downloading gensen model nli_large_bothskip_parse ..."
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse_vocab.pkl
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse.model
-echo "downloading gensen model nli_large_bothskip ..."
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_vocab.pkl
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip.model
-echo "downloading gensen model nli_large ..."
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_vocab.pkl
-wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large.model
+
+echo "downloading gensen models..."
+
+if [[ ! -f "./nli_large_bothskip_2layer_vocab.pkl" ]]
+then
+    echo "downloading gensen model nli_large_bothskip_2layer vocab..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer_vocab.pkl
+fi
+if [[ ! -f "./nli_large_bothskip_2layer.model" ]]
+then
+    echo "downloading gensen model nli_large_bothskip_2layer model..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_2layer.model
+fi
+
+if [[ ! -f "./nli_large_bothskip_parse_vocab.pkl" ]]
+then
+    echo "downloading gensen model nli_large_bothskip_parse_vocab.pkl..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse_vocab.pkl
+fi
+if [[ ! -f "./nli_large_bothskip_parse.model" ]]
+then
+    echo "downloading gensen model nli_large_bothskip_parse.model..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_parse.model
+fi
+
+if [[ ! -f "./nli_large_bothskip_vocab.pkl" ]]
+then
+    echo "downloading gensen model nli_large_bothskip_vocab.pkl..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip_vocab.pkl
+fi
+if [[ ! -f "./nli_large_bothskip.model" ]]
+then
+    echo "downloading gensen model nli_large_bothskip.model..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_bothskip.model
+fi
+
+if [[ ! -f "./nli_large_vocab.pkl" ]]
+then
+    echo "downloading gensen model nli_large_vocab.pkl..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large_vocab.pkl
+fi
+if [[ ! -f "./nli_large.model" ]]
+then
+    echo "downloading gensen model nli_large.model..."
+    wget -q https://genseniclr2018.blob.core.windows.net/models/nli_large.model
+fi
+
 echo "done."
